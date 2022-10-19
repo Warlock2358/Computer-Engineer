cc8b44ff461662686cfa064337fa1e44aa48afd9
Warlock2358 <63557128+Warlock2358@users.noreply.github.com>
2022-10-19 15:25:54 +0200

    Oppgave 8

--- src/Oving8/Person.java
+++ src/Oving8/Person.java
@@ -5,25 +5,40 @@ public class Person {
     private final String lastname;
     private final int yearOfBirth;
 
+
+    /**
+     * Constructor for Person
+     * @param firstname First name of the person
+     * @param lastname Last name of the person
+     * @param yearOfBirth Year of birth of the person
+     */
     public Person(String firstname, String lastname, int yearOfBirth) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.yearOfBirth = yearOfBirth;
     }
 
+    /**
+     * Makes the variable firstname accessible outside the class
+     * @return the firstname
+     */
     public String getFirstname() {
         return firstname;
     }
 
+    /**
+     * Makes the variable lastname accessible outside the class
+     * @return the lastname
+     */
     public String getLastname() {
         return lastname;
     }
 
+    /**
+     * Makes the variable yearOfBirth accessible outside the class
+     * @return the yearOfBirth
+     */
     public int getYearOfBirth() {
         return yearOfBirth;
-    }
-
-    public String toString() {
-        return firstname + " " + lastname + " (" + yearOfBirth + ")";
     }
 }
